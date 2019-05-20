4521296fe3d7dce7aa89e4373be45255d1f86947
Felipe <felipe.lavandeira@gmail.com>
2019-05-20 20:19:33 -0300

    Versao inicial

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/envinfo', function(){
+    echo env('DB_CONNECTION') . "<br />";
+    echo env('DB_HOST') . "<br />";
+    echo env('DB_PORT') . "<br />";
+    echo env('DB_DATABASE') . "<br />";
+    echo env('DB_USERNAME') . "<br />";
+    echo env('DB_PASSWORD') . "<br />";
+    return;
+});
